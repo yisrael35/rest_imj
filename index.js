0ed5670218939f1e62ae0c91c75829417036b5a3
Yisrael Bar <yisrael35@gmail.com>
2021-11-07 07:27:58 +0200

    added to index 'bid' end-point

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const user_routes = require('./api/user/user.routes')
 const forgot_password_routes = require('./api/forgot_password/forgot_password.routes')
 const event_routes = require('./api/event/event.routes')
 const client_routes = require('./api/client/client.routes')
+const bid_routes = require('./api/bid/bid.routes')
 
 // Routes
 server.use('/auth', auth_routes)
@@ -20,6 +21,7 @@ server.use('/user', user_routes)
 server.use('/forgot_password', forgot_password_routes)
 server.use('/event', event_routes)
 server.use('/client', client_routes)
+server.use('/bid', bid_routes)
 
 const port = process.env.APP_PORT || 3001
 server.listen(port, () => {
