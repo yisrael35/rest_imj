4b39739f9ca630388dfe3b5b3e8442fb72935c12
Yisrael Bar <yisrael35@gmail.com>
2022-03-11 17:56:55 +0200

    fix bug in tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -8,7 +8,18 @@ const url = process.env.TEST_URL
 let username = 'yisrael@gmail.com'
 const password = process.env.PASSWORD_TEST
 
-// username = process.env.USERNAME_TEST
+const generate_password = (length) => {
+  var result = ''
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var characters_length = characters.length
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters_length))
+  }
+  return result
+}
+
+const random_name = generate_password(10)
+
 test('test of signin fields', async () => {
   try {
     const result = await axios.put(url + '/auth', { username, password })
@@ -23,7 +34,6 @@ test('test of signin fields', async () => {
   }
 })
 
-const random_name = generate_password(10)
 test('test of signup', async () => {
   try {
     let new_user = { username: random_name, password, first_name: random_name + 'test', last_name: random_name + 'test1', email: random_name + '@imj.org.il' }
@@ -54,12 +64,3 @@ test('test of signout', async () => {
   const result = await axios.delete(url + '/auth')
   expect(result.status).toBe(200)
 })
-const generate_password = (length) => {
-  var result = ''
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  var characters_length = characters.length
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters_length))
-  }
-  return result
-}
