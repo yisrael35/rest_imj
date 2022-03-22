d03df3ae6533712d3d04c34bc38b2dc87f2d15bc
Yisrael Bar <yisrael35@gmail.com>
2022-03-22 20:38:35 +0200

    send sms

---
+++ utils/send_sms.js
@@ -0,0 +1,78 @@
+const Logger = require('logplease')
+const logger = Logger.create('./utils/send_sms.js')
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken)
+const { check_phone } = require('./helper')
+
+const send_sms = async (phone_number, message) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!check_phone(phone_number)) {
+        return reject({ status: 400 })
+      }
+      client.messages
+        .create({
+          body: message,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: phone_number,
+        })
+        .then((message) => {
+          return resolve({
+            status: 200,
+          })
+        })
+        .catch((error) => {
+          logger.error(`Failed to send sms, error: ${error}`)
+          return reject({
+            status: error.status,
+          })
+        })
+    } catch (error) {
+      logger.error(`Failed to send sms, error: ${error}`)
+      if (error.status) {
+        return reject({ status: 400,  })
+      }
+      return reject({
+        status: 500,
+        message: `Server error: ${error.message}`,
+      })
+    }
+  })
+}
+
+module.exports = { send_sms }
+
+// process payload to query string
+const process_payload = (payload) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const processed_payload = {}
+      for (const [key, val] of Object.entries(payload)) {
+        if (val !== undefined || typeof val !== 'object') {
+          switch (key) {
+            case config_process.send_sms.phone:
+              const dialing_code = val[config_process.send_sms.dialing_code].trim()
+              const number = val[config_process.send_sms.number].trim()
+
+              if (dialing_code && number) {
+                processed_payload.phone = '+' + dialing_code + number
+              } else {
+                return reject({ status: 400 })
+              }
+              break
+            case config_process.send_sms.message:
+              processed_payload.message = val.toUpperCase().trim()
+              break
+            default:
+              return reject({ status: 400 })
+          }
+        }
+      }
+      return resolve(processed_payload)
+    } catch (error) {
+      logger.error(`Failed to process payload, The error: ${error}`)
+      return reject({ status: 400 })
+    }
+  })
+}
